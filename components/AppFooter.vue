6bb721c16cc4f685dcc6b0c33ee4c1a83f5cf5ce
Mark Valles <mvalles36@users.noreply.github.com>
2019-09-06 16:36:10 -0500

    Update AppFooter.vue

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,25 +1,14 @@
 <template>
   <footer>
     <section class="callout">
-      <h2>"The surprising styles of Skyline Ivy are advanced for all seasons."</h2>
-      <p>Hansel Andersen</p>
+      <h2>"The surprising Styles of Allgo Are Advanced For All Seasons."</h2>
+      <p>Michael Reddings</p>
     </section>
     <app-footer-links />
     <div class="sarahstuff">
-      <p>
-        This project is
-        <a href="https://github.com/sdras/ecommerce-netlify">open source on github</a>,
-        hosted with
-        <a href="https://bit.ly/2G29YwK">Netlify</a>, and made with love by Sarah Drasner,
-        <a
-          href="https://twitter.com/sarah_edo"
-        >@sarah_edo (twitter)</a> &
-        <a href="https://github.com/sdras">@sdras (github)</a>
-      </p>
     </div>
   </footer>
 </template>
-
 <script>
 import AppFooterLinks from "~/components/AppFooterLinks.vue";
 
