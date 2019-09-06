70699999e87d83e7a0b0a99950e90f6b571d2ee1
Mark Valles <mvalles36@users.noreply.github.com>
2019-09-06 18:13:16 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -52,6 +52,7 @@ exports.handler = async (event, context) => {
               amount: data.stripeAmt,
               receipt_email: data.stripeEmail,
               customer: customer.id,
+              source: data.stripeToken,
               description: "Allgo Premuim"
             },
             {
